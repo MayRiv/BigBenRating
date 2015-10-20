2ed478f7c65927c6998f806b07736c67ebbfab76
Igor Maydanovskiy <mayriv@dev.mi6.kiev.ua>
2015-10-20 13:30:05 +0300

    First player shot if view under the table in GameView.

--- js/bigbenrating.js
+++ js/bigbenrating.js
@@ -7,6 +7,14 @@ var bbr = (function() {
     trySend: function() {
 		alert(1) 
 
+	},
+	checkRoles: function(){
+		var arr = [];
+		for (i=0; i < 10; i++){
+			var role = document.getElementsByName('name')[whole_number].value
+			arr.push(role)
+			alert(role)
+		}
 	}
   }
 
