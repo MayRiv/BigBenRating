78f1e8391698b37dc5a56bf20b3d0af74c291beb
mayriv <mayriv@i.ua>
2015-10-21 10:18:17 +0300

    Added  js checking for roles.

--- js/bigbenrating.js
+++ js/bigbenrating.js
@@ -1,20 +1,37 @@
 var bbr = (function() {
 
 	var version;
-	
+	function checkRoles()
+	{
+		var arr = [];
+		countMaf = 0;
+		countSherif = 0;
+		countDon = 0;
+		countCit = 0;
+		for (i=0; i < 10; i++){
+			var role = document.getElementById('Role'+i).value
+			if(role=="Don")
+				countDon++;
+			if(role=="Citizen")
+				countCit++;
+			if(role=="Mafia")
+				countMaf++;
+			if(role=="Sherif")
+				countSherif++;
+			
+		}
+		if (countSherif == 1 && countDon == 1 && countCit == 6 && countMaf == 2)
+		return true;
+		else return false;
+	}
 
   return {
     trySend: function() {
-		alert(1) 
+		if (checkRoles())
+			document.getElementById('form').submit()
+		else
+			alert("Invalid Roles");
 
-	},
-	checkRoles: function(){
-		var arr = [];
-		for (i=0; i < 10; i++){
-			var role = document.getElementsByName('name')[whole_number].value
-			arr.push(role)
-			alert(role)
-		}
 	}
   }
 
