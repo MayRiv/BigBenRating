fbb438b2a21bcfca643a198000067c67695039ac
Igor Maydanovskiy <mayriv@dev.mi6.kiev.ua>
2015-11-04 15:40:10 +0200

    Commit connection.1

--- rating.php
+++ rating.php
@@ -17,7 +17,7 @@ define ("DEV", 0);
 	}	
 	DBManager::getInstance()->connect($host, $dbName, $user, $password);
 	$players = SQL("SELECT player, p / t AS r, t FROM ( SELECT player, ( SUM( Won ) *2 + SUM( BestPlayer ) * 0.5 + SUM( BestMove ) * 0.25) p, COUNT( * ) t FROM PlayerGame JOIN Players ON ( player = Name ) WHERE StatusId !=2 GROUP BY player ) s WHERE t >10  ORDER BY r DESC")->getAll();
-	foreach ($$players as $p) {
+	foreach ($players as $p) {
 		SQL("INSERT IGNORE INTO `Rating`(`Player`, `Rating`, `GameCount`, `Date`) VALUES (?,?,?,?)", array($p['player'], $p['r'], $p['t'], date("Y-m-d")));
 	}
 ?>
