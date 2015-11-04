a9c39519bbced239d840039f565b2facc4c70f6f
Igor Maydanovskiy <mayriv@dev.mi6.kiev.ua>
2015-11-04 15:36:37 +0200

    Cron task for rating update.

---
+++ rating.php
@@ -0,0 +1,7 @@
+<?php
+require("DBManager.inc");
+$players = SQL("SELECT player, p / t AS r, t FROM ( SELECT player, ( SUM( Won ) *2 + SUM( BestPlayer ) * 0.5 + SUM( BestMove ) * 0.25) p, COUNT( * ) t FROM PlayerGame JOIN Players ON ( player = Name ) WHERE StatusId !=2 GROUP BY player ) s WHERE t >10  ORDER BY r DESC")->getAll();
+foreach ($$players as $p) {
+	SQL("INSERT IGNORE INTO `Rating`(`Player`, `Rating`, `GameCount`, `Date`) VALUES (?,?,?,?)", array($p['player'], $p['r'], $p['t'], date("Y-m-d")));
+}
+?>
