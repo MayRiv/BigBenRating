03dd6bb2df4364ad13f46e10bdf711fa351f5433
Igor Maydanovskiy <mayriv@dev.mi6.kiev.ua>
2015-11-17 11:46:17 +0200

    Rewrite log showing

--- history.php
+++ history.php
@@ -1,6 +1,10 @@
 <?php
+	$fp = fopen("history.log", "r");
+	echo "<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8' /></head><body><div id='feed'>";
+	while(!feof($fp)) { 
+	   	echo fgets($fp);
+	}
+	echo "</div></body></html>";
+	fclose(fp);
 
-	require_once("Logger.inc");
-	Logger::getInstance()->show();
-	echo "1";
 ?>
