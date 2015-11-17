086a1744ac7b8cdef8e13433be060f35509aed9e
Igor Maydanovskiy <mayriv@dev.mi6.kiev.ua>
2015-11-17 11:47:01 +0200

    Show log file.

--- history.php
+++ history.php
@@ -2,9 +2,9 @@
 	$fp = fopen("history.log", "r");
 	echo "<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8' /></head><body><div id='feed'>";
 	while(!feof($fp)) { 
-	   	echo fgets($fp);
+	   	echo fgets($fp) . "<br />";
 	}
 	echo "</div></body></html>";
-	fclose(fp);
+	fclose($fp);
 
 ?>
