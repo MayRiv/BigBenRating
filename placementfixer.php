479e35a94b0a7fddf1905cf51c7d567690e4a667
Igor Maydanovskiy <mayriv@dev.mi6.kiev.ua>
2015-12-04 15:22:35 +0200

    Saved.

--- placementfixer.php
+++ placementfixer.php
@@ -1,14 +1,16 @@
 <?php
-$host = "mysql.hostinger.com.ua";
+/*$host = "mysql.hostinger.com.ua";
 $dbName = "u825515718_bbrat";
 $user = "u825515718_bbrat";
-$password = "B1gBenMafia";
+$password = "B1gBenMafia";*/
+$host = "mysql.hostinger.com.ua"; 
+		$dbName = "u833869977_bbrat"; 
+		$user = "u833869977_bbrat"; 
+		$password = "konoplya_1";
 DBManager::getInstance()->connect($host, $dbName, $user, $password);
 $games = SQL("select GameId from Games")->getAll();
 foreach ($games as $game) {
 	SQL("set @i = 0;Update PlayerGame SET place= @i:=@i +1 where Game=?",array($game["GameId"]));
 }
-
-
 
 ?>
