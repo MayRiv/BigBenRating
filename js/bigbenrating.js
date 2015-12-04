588769f425ad5a369961f1ba560e17cad8bb1e02
Igor Maydanovskiy <mayriv@dev.mi6.kiev.ua>
2015-12-04 13:09:14 +0200

    Trying to get domination via get.

--- js/bigbenrating.js
+++ js/bigbenrating.js
@@ -63,7 +63,7 @@ var bbr = (function() {
 		//form = document.getElementById('form');
 		//form.action = "?action=getDomination&ComparePlayer1=" + encodeURIComponent(document.getElementById('ComparePlayer1')) + "&ComparePlayer2=" + encodeURIComponent(document.getElementById('ComparePlayer2')) //.submit()encodeURIComponent
 		//form.submit();
-		document.location.href = "?action=getDomination&ComparePlayer1=" + encodeURIComponent(document.getElementById('ComparePlayer1')) + "&ComparePlayer2=" + encodeURIComponent(document.getElementById('ComparePlayer2')) //.submit()encodeURIComponent
+		document.location.href = "?action=getDomination&ComparePlayer1=" + encodeURIComponent(document.getElementById('ComparePlayer1').value) + "&ComparePlayer2=" + encodeURIComponent(document.getElementById('ComparePlayer2').value) //.submit()encodeURIComponent
 		
 	}
 }
