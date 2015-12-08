36cfacdce015abefb57be4310ac15e9de3c4e464
Igor Maydanovskiy <mayriv@dev.mi6.kiev.ua>
2015-12-08 16:16:04 +0200

    Added script to show players.

--- showplayergames.php
+++ showplayergames.php
@@ -5,10 +5,11 @@ $dbName = "u825515718_bbrat";
 $user = "u825515718_bbrat";
 $password = "B1gBenMafia";
 DBManager::getInstance()->connect($host, $dbName, $user, $password);
-
+echo "<html><body>"
 $res = SQL("Select player as Name, game as Id from PlayerGame group by Name order by player")->getAll();
 var_dump($res);
 foreach ($res as  $player) {
 	echo "<p><a href=showGame&GameId=".$player['Id'].">".$player['Player']."</a></p>";
 }
+echo "</body></html>"
 ?>
