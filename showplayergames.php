1595e7cc6d00cb1d2c876b25942b2b28523cf2d3
Igor Maydanovskiy <mayriv@dev.mi6.kiev.ua>
2015-12-08 16:20:23 +0200

    Div.

--- showplayergames.php
+++ showplayergames.php
@@ -7,10 +7,10 @@ $dbName = "u825515718_bbrat";
 $user = "u825515718_bbrat";
 $password = "B1gBenMafia";
 DBManager::getInstance()->connect($host, $dbName, $user, $password);
-echo "<html><body>";
+echo "<html><body><div id='feed'>";
 $res = SQL("Select player as Name, game as Id from PlayerGame group by Name order by player")->getAll();
 foreach ($res as  $player) {
 	echo "<p><a href=showGame&GameId=".$player['Id'].">".$player['Name']."</a></p>";
 }
-echo "</body></html>";
+echo "</div></body></html>";
 ?>
