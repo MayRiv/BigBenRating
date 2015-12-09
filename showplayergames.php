41b5baae457eb5ae0b7eaf71c231700862f200d2
Igor Maydanovskiy <mayriv@dev.mi6.kiev.ua>
2015-12-09 10:11:11 +0200

    Fixed editing.

--- showplayergames.php
+++ showplayergames.php
@@ -7,10 +7,13 @@ $dbName = "u825515718_bbrat";
 $user = "u825515718_bbrat";
 $password = "B1gBenMafia";
 DBManager::getInstance()->connect($host, $dbName, $user, $password);
+
+
+
 echo "<html><body><div id='feed' style='margin-left: 300px;'><div style='text-align:left'>";
 $res = SQL("Select player as Name, game as Id from PlayerGame group by Name order by player")->getAll();
 foreach ($res as  $player) {
-	echo "<p><a href='/?action=showGame&GameId=".$player['Id']."'>".$player['Name']."</a></p>";
+	echo "<p><a href='/?action=editGame&GameId=".$player['Id']."'>".$player['Name']."</a></p>";
 }
 echo "</div></div></body></html>";
 ?>
