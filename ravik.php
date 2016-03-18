4251ea1e56c84663d83bc64f69b66eeebe97a65e
Igor Maydanovskiy <mayriv@dev.mi6.kiev.ua>
2016-03-18 10:51:20 +0200

    Added ravik.php

--- ravik.php
+++ ravik.php
@@ -9,7 +9,7 @@ $password = "konoplya_1";
 DBManager::getInstance()->connect($host, $dbName, $user, $password);
 
 echo "<html><body><div id='feed' style='margin-left: 300px;'><div style='text-align:left'>";
-$res = SQL("  SELECT player as Name, COUNT( * ) as t FROM PlayerGame JOIN Games ON ( Game = GameId ) JOIN Players ON (player = Name ) WHERE StatusId !=2 AND StatusId !=0 AND DATE BETWEEN CAST(  2015-12-16 AS DATE )  AND CAST( 2016-3-16 AS DATE )  GROUP BY player  ORDER BY  t DESC")->getAll();
+$res = SQL("  SELECT player as Name, COUNT( * ) as t FROM PlayerGame JOIN Games ON ( Game = GameId ) JOIN Players ON (player = Name ) WHERE StatusId !=2 AND StatusId !=0 AND DATE BETWEEN CAST(  '2015-12-16' AS DATE )  AND CAST( '2016-3-16' AS DATE )  GROUP BY player  ORDER BY  t DESC")->getAll();
 foreach ($res as  $player) {
 	echo "<p>".$player['Name'].":".$player['t']."</p>";
 }
