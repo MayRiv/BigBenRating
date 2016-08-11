2af2d7bb37fe916ffebd2c6a9d119ce9514fd8e6
Igor Maydanovskiy <mayriv@dev.mi6.kiev.ua>
2016-08-11 11:19:05 +0300

    Show players non-orgs.

--- showplayers.php
+++ showplayers.php
@@ -10,8 +10,10 @@ DBManager::getInstance()->connect($host, $dbName, $user, $password);
 
 echo "<html><body><div id='feed' style='margin-left: 300px;'><div style='text-align:left'>";
 $res = SQL("Select  Name from Players where StatusID = 1 order by Name ASC")->getAll();
+$i = 1;
 foreach ($res as  $player) {
-	echo "<p>".$player['Name']."</p>";
+	echo "<p>$i) ".$player['Name']."</p>";
+	$i++;
 }
 echo "</div></div></body></html>";
 ?>
